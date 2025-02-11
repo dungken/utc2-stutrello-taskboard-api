1cf4f8a1176315e9381216f89c9aa0fe4e865fc2
Van Dung Dev <vincentcode402112@gmail.com>
2025-02-11 20:16:45 +0700

    connect mongodb env variables

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -3,3 +3,37 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+const MONGODB_URI = 'mongodb+srv://dungkendev:EdcwlgZySjCp0Krs@cluster0.gzxim.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const DATABASE_NAME = 'trello-dungken-database-mern-stack'
+
+const { MongoClient, ServerApiVersion } = require('mongodb')
+
+// Khoi tao mot doi tuong trelloDatabaseInstance la null de thao tac voi database (vi chua ket noi)
+let trelloDatabaseInstance = null
+
+// Khoi tao mot doi tuong mongoClientInstance de ket noi voi MongoDB
+const mongoClientInstance = new MongoClient(MONGODB_URI, {
+  // Luu y: serverApi phai duoc cung cap de su dung cac tinh nang moi nhat cua MongoDB
+  // Doc them: https://www.mongodb.com/docs/drivers/node/current/fundamentals/stable-api/
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+
+// Ket noi toi Database
+export const CONNECT_DB = async () => {
+  // Goi ket noi toi MongoDB Atlas voi URI da khai bao trong than cua mongoClientInstance
+  await mongoClientInstance.connect()
+
+  // Ket noi thanh cong thi lay ra Database theo ten va gan nguoc no lai vao bien trelloDatabaseInstance
+  trelloDatabaseInstance = mongoClientInstance.db(DATABASE_NAME)
+}
+
+// Function GET_DB (khong async) de lay ra doi tuong trelloDatabaseInstance sau khi da ket noi thanh cong voi MongoDB de co the su dung trong cac file khac
+// Luu y: Phai ket noi voi MongoDB truoc khi su dung ham nay
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance) throw new Error('Must connect to database first!')
+  return trelloDatabaseInstance
+}
--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /**
  * Updated by trungquandev.com's author on August 17 2023
  * YouTube: https://youtube.com/@trungquandev
@@ -5,29 +6,46 @@
  */
 
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import { CONNECT_DB, GET_DB } from '~/config/mongodb'
 
-const app = express()
+const START_SERVER = () => {
+  const app = express()
 
-const hostname = 'localhost'
-const port = 8017
+  const hostname = 'localhost'
+  const port = 8017
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  // eslint-disable-next-line no-console
-  console.log(mapOrder(
-    [{ id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' }],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<p>Hello World!</p><hr>')
-})
+  app.get('/', async (req, res) => {
+    console.log(await GET_DB().listCollections().toArray())
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
-})
+    res.end('<p>Hello World!</p><hr>')
+  })
+
+  app.listen(port, hostname, () => {
+    console.log(`3. Hello Dung Dev, I am running at http://${ hostname }:${ port }/`)
+  })
+}
+
+// Chi khi ket noi voi MongoDB thanh cong thi moi bat dau chay server
+// IIFE (Immediately Invoked Function Expression)
+(async () => {
+  try {
+    console.log('1. Connecting to MongoDB Cloud Atlas...')
+    await CONNECT_DB()
+    console.log('2. Connected to MongoDB successfully!')
+    // Sau khi ket noi thanh cong voi MongoDB thi moi bat dau chay server
+    START_SERVER()
+  } catch (error) {
+    console.error(error)
+    process.exit(0)
+  }
+})()
+
+// Chi khi ket noi voi MongoDB thanh cong thi moi bat dau chay server
+// console.log('1. Connecting to MongoDB Cloud Atlas...')
+// CONNECT_DB()
+//   .then(() => console.log('2. Connected to MongoDB successfully!'))
+//   .then(() => START_SERVER())
+//   .catch(error => {
+//     console.error(error)
+//     process.exit(0)
+//   })
