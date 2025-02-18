4663130de7f96ef15911772855fec01dd421a0a2
dungken <vincentcode402112@gmail.com>
2025-02-18 08:29:21 +0700

    Update WHITELIST_DOMAINS

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
 export const WHITELIST_DOMAINS = [
+  'https://trellox-web.vercel.app'
   // 'http://localhost:5173' // Khong can localhost nua vi o file/cors da luon cho phep moi truong dev (env.BUILD_MODE === 'dev')
   // sau khi deploy lên production thì domain sẽ thay đổi
 ]
