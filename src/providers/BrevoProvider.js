d8ced3b2927b55a78fc0f6d47aa0709792d1b8aa
dungken <vincentcode402112@gmail.com>
2025-02-19 09:31:49 +0700

    send_mail_with_brevo

---
+++ src/providers/BrevoProvider.js
@@ -0,0 +1,34 @@
+// https://github.com/getbrevo/brevo-node
+const SibApiV3Sdk = require('@getbrevo/brevo')
+import { env } from '~/config/environment'
+
+let apiInstance = new SibApiV3Sdk.TransactionalEmailsApi()
+let apiKey = apiInstance.authentications['apiKey']
+apiKey.apiKey = env.BREVO_API_KEY
+console.log('🚀 ~ env.BREVO_API_KEY:', env.BREVO_API_KEY)
+
+const sendEmail = async (recipientEmail, customSubject, htmlContent) => {
+  // Khoi tao mot cai sendSmtpEmail voi nhung thong tin can thiet
+  let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail()
+
+  // Tai khoan gui mail: la tao khoan tren Brevo
+  sendSmtpEmail.sender = { email: env.ADMIN_EMAIL_ADDRESS, name: env.ADMIN_EMAIL_NAME }
+
+  // Nhung tai khoan nhan email
+  // 'to' la mot Array de sau co the tuy bien gui 1 email toi nhieu user
+  sendSmtpEmail.to = [{ email: recipientEmail }]
+
+  // Tieu de cua email
+  sendSmtpEmail.subject = customSubject
+
+  // Noi dung cua email
+  sendSmtpEmail.htmlContent = htmlContent
+
+  // Goi hanh dong gui email
+  // More info: thanh sendTransacEmail cua thu vien no se return mot Promise
+  return apiInstance.sendTransacEmail(sendSmtpEmail)
+}
+
+export const BrevoProvider = {
+  sendEmail
+}
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,5 @@
+import { env } from '~/config/environment'
+
 export const WHITELIST_DOMAINS = [
   'https://trellox-web.vercel.app'
   // 'http://localhost:5173' // Khong can localhost nua vi o file/cors da luon cho phep moi truong dev (env.BUILD_MODE === 'dev')
@@ -8,3 +10,7 @@ export const BOARD_TYPES = {
   PUBLIC: 'public',
   PRIVATE: 'private'
 }
+
+export const WEBSITE_DOMAIN = (env.BUILD_MODE === 'prod')
+  ? env.WEBSITE_DOMAIN_PRODUCTION
+  : env.WEBSITE_DOMAIN_DEVELOPMENT
