8a2388b682769fe30ca3c2542361f07b4da8032b
dungken <vincentcode402112@gmail.com>
2025-02-19 22:43:31 +0700

    handle_board_list_page

--- src/routes/v1/boardRoute.js
+++ src/routes/v1/boardRoute.js
@@ -1,5 +1,4 @@
 import express from 'express'
-import { StatusCodes } from 'http-status-codes'
 import { boardValidation } from '~/validations/boardValidation'
 import { boardController } from '~/controllers/boardController'
 import { authMiddleware } from '~/middlewares/authMiddleware'
@@ -9,9 +8,7 @@ const Router = express.Router()
 export const boardRoute = Router
 
 Router.route('/')
-  .get((req, res) => {
-    res.status(StatusCodes.OK).json({ message: 'GET: API get list boards' })
-  })
+  .get(authMiddleware.isAuthorized, boardController.getBoards)
   .post(authMiddleware.isAuthorized, boardValidation.createNew, boardController.createNew)
 
 Router.route('/:id')
