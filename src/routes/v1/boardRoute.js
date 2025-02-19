77c857feb1f94bfa06e84877cea2254a64f7458b
dungken <vincentcode402112@gmail.com>
2025-02-19 14:24:16 +0700

    httpOnly_cookie_authMiddleware_redux_persist_protected_routes

--- src/routes/v1/boardRoute.js
+++ src/routes/v1/boardRoute.js
@@ -2,6 +2,7 @@ import express from 'express'
 import { StatusCodes } from 'http-status-codes'
 import { boardValidation } from '~/validations/boardValidation'
 import { boardController } from '~/controllers/boardController'
+import { authMiddleware } from '~/middlewares/authMiddleware'
 
 const Router = express.Router()
 
@@ -11,14 +12,14 @@ Router.route('/')
   .get((req, res) => {
     res.status(StatusCodes.OK).json({ message: 'GET: API get list boards' })
   })
-  .post(boardValidation.createNew, boardController.createNew)
+  .post(authMiddleware.isAuthorized, boardValidation.createNew, boardController.createNew)
 
 Router.route('/:id')
-  .get(boardController.getDetails)
-  .put(boardValidation.update, boardController.update)
+  .get(authMiddleware.isAuthorized, boardController.getDetails)
+  .put(authMiddleware.isAuthorized, boardValidation.update, boardController.update)
 
 // API ho tro viec di chuyen card giua cac column khac nhau trong 1 board
 Router.route('/supports/moving_card')
-  .put(boardValidation.moveCardToDifferentColumn, boardController.moveCardToDifferentColumn)
+  .put(authMiddleware.isAuthorized, boardValidation.moveCardToDifferentColumn, boardController.moveCardToDifferentColumn)
 
 export const APIs_V1 = Router
--- src/routes/v1/cardRoute.js
+++ src/routes/v1/cardRoute.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import { cardValidation } from '~/validations/cardValidation'
 import { cardController } from '~/controllers/cardController'
+import { authMiddleware } from '~/middlewares/authMiddleware'
 
 const Router = express.Router()
 
 export const cardRoute = Router
 
 Router.route('/')
-  .post(cardValidation.createNew, cardController.createNew)
+  .post(authMiddleware.isAuthorized, cardValidation.createNew, cardController.createNew)
 
 export const APIs_V1 = Router
