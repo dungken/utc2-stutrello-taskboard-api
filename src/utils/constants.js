6d702788b723345ccb5d22de00f6e705250fa9bb
dungken <vincentcode402112@gmail.com>
2025-02-20 09:11:52 +0700

    config domain on production

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,7 +5,7 @@ export const WHITELIST_DOMAINS = [
   // 'http://localhost:5173' // Không cần localhost nữa vì ở file config/cors đã luôn luôn cho phép môi trường dev (env.BUILD_MODE === 'dev')
 
   // Lưu ý: Đây là domain ví dụ sau khi Deploy Production
-  'https://trello-web-mu.vercel.app'
+  'https://utc2-stutrello-taskboard-web.vercel.app'
 ]
 
 export const BOARD_TYPES = {
